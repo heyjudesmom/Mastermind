3a579257f5ab834a476ff4b9d2322a5270be58a5
heyjudesmom <brianna.g.mccarthy@gmail.com>
2022-07-01 15:54:57 -0700

    basic mock up HTML and CSS

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 /*----- constants -----*/
 /*----- app's state (variables) -----*/
+let gameStatus;
+let board;
+let secret;
 /*----- cached element references -----*/
+const rows = [...document.querySelectorAll('#board > div')]
 /*----- event listeners -----*/
 /*----- functions -----*/
