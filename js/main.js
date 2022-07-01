3497707936c0de6916d784353d0c97496e108325
heyjudesmom <brianna.g.mccarthy@gmail.com>
2022-07-01 16:25:15 -0700

    colors

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
 /*----- constants -----*/
+const COLORS = [
+    'var(--red)', 
+    'var(--orange)',
+    'var(--yellow)',
+    'var(--green)',
+    'var(--blue)',
+    'var(--purple)',
+    'var(--indigo)',
+    'var(--brown)',
+]
 /*----- app's state (variables) -----*/
 let gameStatus;
 let board;
 let secret;
 /*----- cached element references -----*/
-const rows = [...document.querySelectorAll('#board > div')]
+const rowEls = [...document.querySelectorAll('#board > div')]
+const feedbackEls = document.querySelector('.f > div')
 /*----- event listeners -----*/
 /*----- functions -----*/
