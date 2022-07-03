edf1d5ace8e9f12c17d27b9d7b08ae29d7343d7b
heyjudesmom <brianna.g.mccarthy@gmail.com>
2022-07-02 18:36:34 -0700

    generated and rendered secret code

--- js/main.js
+++ js/main.js
@@ -9,12 +9,50 @@ const COLORS = [
     'var(--indigo)',
     'var(--brown)',
 ]
+const slots =[1, 2, 3, 4];
 /*----- app's state (variables) -----*/
-let gameStatus;
-let board;
-let secret;
+let secrets = []; //Array to hold randomly selected colors
+let gameStatus; //true :game in play, false: secret revealed
+let guess = [];//array of current guessed colors
+let feedback; //array to hold the feedback pegs
+
 /*----- cached element references -----*/
-const rowEls = [...document.querySelectorAll('#board > div')]
-const feedbackEls = document.querySelector('.f > div')
+const rowEls = [...document.querySelectorAll('#board > div')];
+const secretEls = [...document.querySelectorAll('#secret > div')]
+// const feedbackEls = document.querySelectorAll('.f > div');
+//TODO: cache the messageEl to declare win or loss
+
 /*----- event listeners -----*/
+
+
 /*----- functions -----*/
+init();
+
+function init() {
+    generateSecret(); //Step 1. Done
+    render();
+};
+
+function generateSecret() { //For Step 1. Done
+   slots.map(function(){
+        return secrets.push(COLORS[Math.floor(Math.random() * COLORS.length)])
+   })
+}
+function render() {
+    renderSecret(); //For Step 1A. Done
+  //  renderChoice();
+}
+// function renderChoice() {
+//     evt.target.style.border = '0.5vmin dashed hotpink'
+// }
+function renderSecret(){
+    secretEls.forEach(function(div, idx) {
+      div.style.backgroundColor = secrets[idx];
+    })
+};
+//function getColor(evt) {
+//     //guards
+//     if (evt.target !== document.querySelectorAll('.bank > div')) {
+//         return
+//     } return evt.target.style.backgroundColor;
+// }
