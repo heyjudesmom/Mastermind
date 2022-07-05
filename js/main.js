a307f19b4e1de29e85593d735706d21324cfe166
heyjudesmom <brianna.g.mccarthy@gmail.com>
2022-07-05 16:36:02 -0700

    Checker can check full secret

--- js/main.js
+++ js/main.js
@@ -11,31 +11,35 @@ const COLORS = [
 ]
 const SECRET_SLOTS =[1, 2, 3, 4];
 const MAX_WRONG_GUESSES = 10;
-const board = {
-    1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: [], 9: [], 10:[]
-}
+
 /*----- app's state (variables) -----*/
 let secrets = []; //Array to hold randomly selected colors
 let gameStatus; //true: game in play, false: secret revealed
-let currentChoices = [];
-let wrongGuesses = [];
+let currentChoices = [];//Array to hold bank choices for row
+let rows = [{slot1: '', slot2: '', slot3: '', slot4: ''}];//
+let pegs = [];
 /*----- cached element references -----*/
 const secretEls = [...document.querySelectorAll('#secret > div')];
-const rowEls = [...document.querySelectorAll('.guess')].reverse(); //returns all 10 guess rows 
-const bankColors = [...document.querySelectorAll('#bank > div')];
-const guessEls = [...document.querySelectorAll('.guess > .dropzone')];
+const rowEls = [...document.querySelectorAll('.guess > .dropzone')]; //returns all 10 guess rows 
+const bankEls = [...document.querySelectorAll('#bank > div')];
+bankEls.forEach(function(div, idx) {
+    div.style.backgroundColor = COLORS[idx]
+});
 const checkBtn = document.getElementById('check');
-// const feedbackEls = document.querySelectorAll('.f > div');
+const feedbackEls = document.querySelectorAll('.f > div');
+const playBtn = document.getElementById('play');
 //TODO: cache the messageEl to declare win or loss
 
 /*----- event listeners -----*/
-document.getElementById('bank').addEventListener('click', storeBankClick);
+document.getElementById('bank').addEventListener('click', storeBankClicks);
+document.getElementById('check').addEventListener('click', handleCheck);
+document.getElementById('play').addEventListener('click', handlePlay);
 /*----- functions -----*/
 init();
 
 function init() {
+    gameStatus = true;
     generateSecret(); //Step 1. Done
-    check();
     render();
 };
 
@@ -46,8 +50,18 @@ function generateSecret() { //For Step 1. Done
 }
 function render() {
     renderSecret(); //For Step 1A. Done
-      //Render Check Button  checkBtn.style.visibility = currentChoice === 4 ? 'visible' : 'hidden';
+    renderBankClicks();
+    renderCheckBtn();
+}
 
+function renderCheckBtn() {
+    checkBtn.style.visibility = currentChoices.length === 4 ? 'visible' : 'hidden';
+}
+function renderBankClicks() {
+    //guards
+    rowEls.forEach(function(div, idx) {
+        div.style.backgroundColor = currentChoices[idx];
+})
 }
 
 function renderSecret(){ //For step 1A. Done.
@@ -55,17 +69,48 @@ function renderSecret(){ //For step 1A. Done.
       div.style.backgroundColor = secrets[idx];
     })
 };
-function storeBankClick(evt, idx) { //For Step 2a. Done
+function storeBankClicks(evt) { //For Step 2a. Done
     //guards
+    
     if (evt.target === document.querySelector('section')) {
         return
     }
     if (currentChoices.length === SECRET_SLOTS.length) {
-        currentChoices.length = 0;
+        currentChoices = []
+        rowEls.forEach(function(div) {
+            div.style.backgroundColor = null;
+          })
     }
-    return currentChoices.push(evt.target);
+    currentChoices.push(evt.target.style.backgroundColor);
+    render();
 }
-function check() {
-    currentChoices.map(function () {
-    })
+function handleCheck() {
+    if (currentChoices.toString() === secrets.toString()){
+     console.log('hello');
+     gameStatus = false;
+     render();
+    } else if (currentChoices.includes(secrets[0], secrets[1], secrets[2], secrets[3])) {
+        if (secrets.includes(currentChoices[0])) {
+            pegs.push('a')
+        } if (secrets.includes(currentChoices[1])) {
+            pegs.push('b')
+        } if (secrets.includes(currentChoices[2])) {
+            pegs.push('c')
+        } if (secrets.includes(currentChoices[3])) {
+            pegs.push('d')
+        }         renderFeedback()
+    } 
+
+ }
+function renderFeedback() {
+    
 }
+
+function handlePlay() {
+    init();
+}
+// rows[0].slot1 = currentChoices[0]
+// rows[0].slot2 = currentChoices[1]
+// rows[0].slot3 = currentChoices[2]
+// rows[0].slot4 = currentChoices[3]
+// return rows[0]; //not working
